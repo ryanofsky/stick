06e72579c783bb511d982cfa1a905db4bc5cc5a1
Russell Yanofsky <russ@yanofsky.org>
1998-01-15 17:21:50 +0000

    - worked out geometry needed to draw stick figure - added the game loop

--- stick.pas
+++ stick.pas
@@ -63,7 +63,7 @@ var aln,lng:integer;
     TheFont := CreateFontIndirect(PrpFont);
     OldFont := SelectObject(DC, TheFont);
     settextalign(DC,aln);
-    setbkmode(DC,1);  settextcolor(DC,color); textout(DC,x,y,pc(txt),lng); settextcolor(DC,0);
+    settextcolor(DC,color); textout(DC,x,y,pc(txt),lng); settextcolor(DC,0);
     TheFont:=SelectObject(DC, OldFont);
     DeleteObject(TheFont);
     TheFont:=OldFont;
@@ -137,23 +137,114 @@ var x1,y1,x2,y2,c,d:integer;
     Ellipse(DC, X1,Y1,X2,Y2);
   end;
 
-procedure unfreeze;
+procedure qline(DC: HDC; x1,y1,x2,y2:integer);
+var ends:array[1..2] of tpoint;
+  begin
+    ends[1].x:=x1;  ends[1].y:=y1;  ends[2].x:=x2;  ends[2].y:=y2;
+    polyline(DC,ends,2);
+  end;
+
+procedure Qarc(DC: HDC; xpos,ypos,radiusw,radiush,angle1,angle2,way:integer);
+var x1,y1,x2,y2,x3,y3,x4,y4,c,d:integer;
+  begin
+    x1:=xpos-radiusw;  y1:=ypos-radiush;  x2:=xpos+radiusw;  y2:=ypos+radiush;
+    x3:=xpos+round(radiusw*(cos(angle1/180*pi))); y3:=ypos-round(radiush*(sin(angle1/180*pi)));
+    x4:=xpos+round(radiusw*(cos(angle2/180*pi))); y4:=ypos-round(radiush*(sin(angle2/180*pi)));
+    case way of
+      0: arc(DC, X1,Y1,X2,Y2,X3,Y3,X4,Y4);
+      1: chord(DC, X1,Y1,X2,Y2,X3,Y3,X4,Y4);
+      2: pie(DC, X1,Y1,X2,Y2,X3,Y3,X4,Y4);
+    end;
+  end;
+
+procedure unfreeze(Wnd: Hwnd);
 var M: TMsg;
   begin  
-    while PeekMessage(M, 0, 0, 0, pm_Remove) do
+    while PeekMessage(M, Wnd, 0, 0, pm_Remove) do
     begin
       DispatchMessage(M);
     end;
   end;
 
-procedure delay(milliseconds:longint);
+procedure delay(milliseconds:longint; Wnd:Hwnd);
 var t: longint;
   begin
     t:=gettickcount;
     repeat
-    unfreeze;
+    unfreeze(Wnd);
     until gettickcount-t>=milliseconds;
   end;
+
+function atan(x,y:real): real;
+  begin                                 
+    if y <> 0 then
+      if x <> 0 then                  
+        if x > 0 then                 
+          atan := arctan(y/x)
+        else
+          if y > 0 then               
+            atan := pi + arctan(y/x)
+          else
+            atan := arctan(y/x) - pi
+      else                              
+        if y >= 0 then
+          atan := pi/2             
+         else
+          atan := -pi/2           
+     else                               
+      if x >= 0 then
+        atan := 0                  
+       else
+        atan := - Pi                 
+  end;                                
+
+function asin(x: real): real;
+  begin
+    if x = 1 then
+      asin := pi / 2
+    else
+      if x = - 1 then
+        asin := pi / -2
+      else
+        asin := arctan(x / sqrt(1 - sqr(x)));
+  end;
+
+function acos(x: real): real;
+  begin
+
+    { not mathematically correct, but painless way to fix program }
+      if abs(x) > 1 then acos:=pi else
+    { done }
+    if x = 0 then
+      acos := pi / 2
+    else
+      if x < 0 then
+        acos := pi - arctan(sqrt(1 - sqr(X)) / abs(X))
+      else
+        acos := arctan(sqrt(1 - sqr(X)) / abs(X))
+   end;
+
+function min(x,y: real): real;
+  begin
+    if x<y then min:=x else min:=y;
+  end;
+
+function max(x,y: real): real;
+  begin
+    if x>y then max:=x else max:=y;
+  end;
+
+function badtri(A,B,C:real): bool;
+  begin
+    A:=abs(a); B:=abs(b); c:=abs(c);
+    if (a+b<c) or (b+c<a) or (a+c<b) then badtri:=true else badtri:=false;
+  end;
+
+procedure setcurdc(tDC:HDC);
+  begin
+    DC := tDC;
+  end;
+
 
 {- END ------------------------------------------  Drawing Commands}
 
@@ -166,7 +257,7 @@ const fps = 5;
       PinM = 150;
 
 type tkeys = record
-       left,right,up,down,punch,kick:integer;
+       left,right,up,down,punch,kick:word
      end;
 
 type pfprops = ^tfprops;
@@ -178,7 +269,6 @@ type pfprops = ^tfprops;
 
 type pgenf = ^tgenf;
      tgenf = object(Tobject)
-       defheight: integer;
        pos: array [1..fframes] of tfprops;
        keycodes: tkeys;
        DC: HDC;
@@ -206,6 +296,7 @@ constructor tgenf.init(TheDC:HDC; TheWind: Hwnd; xpos,ypos, dir: integer; sze:re
       with pos[x] do
         begin
           l1x  := 0.5;
+          l1y  :=  -1;
           l2x  := 0.5;
           l2y  :=  -1;
           cx   :=   0;
@@ -213,7 +304,8 @@ constructor tgenf.init(TheDC:HDC; TheWind: Hwnd; xpos,ypos, dir: integer; sze:re
           a1x  := 0.5;
           a2x  := 0.5;
           head := 0.5;
-          duck := 0.5;
+          duck := 0.8;
+          size := sze*2;
           x    := xpos;
           y    := ypos;
           direction := dir;
@@ -250,10 +342,12 @@ procedure tgenf.advanceframe;
 
 procedure tgenf.walkr;
   begin
+    pos[1].x:=pos[1].x+10;
   end;
 
 procedure tgenf.walkl;
   begin
+    pos[1].x:=pos[1].x-10;
   end;
 
 procedure tgenf.kick;
@@ -287,56 +381,154 @@ procedure tgenf.setkeys1;
 
 type pstickman = ^tstickman;
      tstickman = object(tgenf)
-       defl1,defl2,defa1,defa2,headdiam: integer;
-       K1x,K1y,K2x,K2y,F1x,F1y,F2x,F2y,Ctx,Cty,E1x,E1y,E2x,E2y,H1x,H1y,H2x,H2y,Hdx,Hdy: integer;
+       defl1,defl2,defa1,defa2,headW,headH,deftorso: integer;
+       K1x,K1y,K2x,K2y,F1x,F1y,F2x,F2y,Ctx,Cty,E1x,E1y,E2x,E2y,H1x,H1y,H2x,H2y,Hdx,Hdy,
+       Sx,Sy: integer;
+       nose: real;
        procedure subinit; virtual;
        procedure draw; virtual;
      end;
 
 procedure tstickman.draw;
+  var duckby,ang,t1x,t1y,t2x,t2y: real;
+      temp: string;
+      a2: real;
   begin
-    setpen(DC,color[4],0,2);
-    setbrush(DC,color[0],color[0],0);
-    qcircle(DC,pos[1].x,pos[1].y,10,10);
+    with pos[1] do
+      begin
+        setcurdc(DC);
+        setbrush(DC,0,0,0);
+        box(DC,0,0,640,480,0,0);
+
+        duckby := duck*(defl1+defl2);
+        cty    := round(y-duckby);
+        ctx    := round(x);
+{ ------------------------------------------  Legs         }
+        t1x := l1x*(defl1+defl2);
+        t1y := l1y*duckby;
+        t2x := l2x*(defl1+defl2);
+        t2y := l2y*duckby;
+
+        f1x    := round(x-direction*t1x);
+        f2x    := round(x+direction*t2x);
+        f1y    := round(y-duckby-t1y);
+        f2y    := round(y-duckby-t2y);
+
+        ang    := atan(t1x,t1y)+pi
+                  -acos((sqr(defl2)-sqr(defl1)-(sqr(t1x)+sqr(t1y)))/
+                  (2*defl1*sqrt(sqr(t1x)+sqr(t1y))));
+        k1x    := round(x-direction*defl1*cos(ang));
+        k1y    := round(y-duckby-defl1*sin(ang));
+        
+        ang    := atan(t2x,t2y)+pi
+                  -acos((sqr(defl2)-sqr(defl1)-(sqr(t2x)+sqr(t2y)))/
+                  (2*defl1*sqrt(sqr(t2x)+sqr(t2y))));
+        k2x    := round(x+direction*defl1*cos(ang));
+        k2y    := round(y-duckby-defl1*sin(ang));
 
 
+{ ------------------------------------------  Torso        }
+        Sx     := ctx;
+        Sy     := cty-deftorso;
+
+{ ------------------------------------------  Arms         }
+
+        a2x:=0.2;
+
+        t1x := a1x*(defa1+defa2);
+        t2x := a2x*(defa1+defa2);
+        t1y := 0;
+        t2y := 0;
+
+        h1x    := round(x-direction*t1x);
+        h2x    := round(x+direction*t2x);
+        h1y    := Sy;
+        h2y    := Sy;
+
+        ang    := -acos((sqr(defa2)-sqr(defa1)-(sqr(t1x)+sqr(t1y)))/
+                  (2*defa1*sqrt(sqr(t1x)+sqr(t1y))));
+        e1x    := round(Sx+direction*defa1*cos(ang));
+        e1y    := round(Sy-defa1*sin(ang));
+        
+        ang    := -acos((sqr(defa2)-sqr(defa1)-(sqr(t2x)+sqr(t2y)))/
+                  (2*defa1*sqrt(sqr(t2x)+sqr(t2y))));
+        e2x    := round(Sx-direction*defa1*cos(ang));
+        e2y    := round(Sy-defa1*sin(ang));
+
+{ ------------------------------------------  Head         }
+        hdx    := sx;
+        hdy    := round(sy-head*PinM*size/2);
+
+        {herehere}
 
 
-{
+    setpen(DC,color[15],0,0);
+    qline(DC,k2x,k2y,f2x,f2y);
+    qline(DC,ctx,cty,k2x,k2y);
+    qline(DC,k1x,k1y,f1x,f1y);
+    qline(DC,ctx,cty,k1x,k1y);
+    qline(DC,ctx,cty,Sx,Sy);
+    qline(DC,Sx,Sy,E1x,E1y);
+    qline(DC,E1x,E1y,H1x,H1y);
+    qline(DC,Sx,Sy,E2x,E2y);
+    qline(DC,E2x,E2y,H2x,H2y);
+    qline(DC,Sx,Sy,Hdx,Hdy);
+    
+    setpen(DC,color[12],0,2);
+    qcircle(DC,ctx,cty,3,3);
+    qcircle(DC,k1x,k1y,3,3);
+    qcircle(DC,k2x,k2y,3,3);
+    qcircle(DC,f1x,f1y,3,3);
+    qcircle(DC,f2x,f2y,3,3);
+    qcircle(DC,Sx,Sy,3,3);
+    qcircle(DC,E1x,E1y,3,3);
+    qcircle(DC,H1x,H1y,3,3);
+    qcircle(DC,E2x,E2y,3,3);
+    qcircle(DC,H2x,H2y,3,3);
+    setbrush(DC,0,0,0);
+    qcircle(DC,HDx,HDy,direction*headw,headh);
+    qline(DC,HDx+round(direction*headw*nose),HDy,
+          HDx+round(direction*headw*cos(20/180*pi)),
+          HDy-round(headh*sin(20/180*pi)));
+    qline(DC,HDx+round(direction*headw*nose),HDy,
+          HDx+round(direction*headw),HDy);
+    {
 
-          l1x  := 0.5;
-          l2x  := 0.5;
-          l2y  :=  -1;
-          cx   :=   0;
-          cy   :=   0;
-          a1x  := 0.5;
-          a2x  := 0.5;
-          head := 0.5;
-          duck := 0.5;
-          x    := xpos;
-          y    := ypos;
-          direction := dir;
-          jump := false;
-          walk := false;
-          punch:= false;
-          kick := false;
-}
+    qarc(DC,HDx+round(direction*headw*0.75),HDy-round(headh*0.3),
+           25,20,160,190,2);
 
 
+    qarc(DC,HDX+round(direction*headw),HDy,
+            round(direction*headw*1),10,190,260,0);
+     }
+   
+      end;
 
-
+    setfont(DC,'Arial',15,9,0,0,0,0);
+    str(t2x:0:0,temp);
+    txt(DC,10,250+10,1,color[15],'t2x   '+temp);
+    str(t2y:0:0,temp);
+    txt(DC,10,250+30,1,color[15],'t2y   '+temp);
+    str(ang/pi*180:0:0,temp);
+    txt(DC,10,250+50,1,color[15],'ang   '+temp);
+    str(k1x:0,temp);
+    txt(DC,10,250+70,1,color[15],'k1x   '+temp);
+    str(k1y:0,temp);
+    txt(DC,10,250+90,1,color[15],'k1y   '+temp);
 
 
   end;
 
 procedure tstickman.subinit;
   begin
-    defheight := round(1.75 * PinM);
-    defl1     := round(0.5 * PinM);
-    defl2     := round(0.5 * PinM);
-    defa1     := round(0.4 * PinM);
-    defa2     := round(0.4 * PinM);
-    headdiam  := round(0.3 * PinM);
+    defl1     := round(0.5  * PinM  * pos[1].size);
+    defl2     := round(0.4  * PinM  * pos[1].size);
+    defa1     := round(0.3  * PinM  * pos[1].size);
+    defa2     := round(0.3  * PinM  * pos[1].size);
+    deftorso  := round(0.4  * PinM  * pos[1].size);
+    headw     := round(0.25 * PinM  * pos[1].size);
+    headh     := round(0.15 * PinM  * pos[1].size);
+    nose      := 1.4;
   end;
 
 type pwind = ^twind;
@@ -438,12 +630,13 @@ procedure Twind.WMKeyDown(var Msg:Tmessage);
             begin
               if msg.wparam = left  then f^.walkl;
               if msg.wparam = right then f^.walkr;
-              if msg.wparam = up    then f^.jump;
-              if msg.wparam = down  then f^.duck;
+              if msg.wparam = up    then f^.jump; 
+              if msg.wparam = down  then f^.duck; 
               if msg.wparam = punch then f^.punch;
-              if msg.wparam = kick  then f^.kick;
+              if msg.wparam = kick  then f^.kick; 
             end;
         end;
+    defwndproc(Msg);
   end;
 
 procedure twind.wmpaint(var msg: tmessage);
@@ -459,8 +652,23 @@ procedure twind.wmpaint(var msg: tmessage);
      end;
 
    procedure fight;
+     var x,fin: integer;
+         f: pgenf;
      begin
-       mode:='fight';
+      fin:=0;
+      mode:='fight';
+      repeat
+        for x:=0 to fpool^.count-1 do
+          begin
+            f:=fpool^.at(x);
+            with f^ do
+              begin
+                unfreeze(HWindow);
+                draw;
+                advanceframe;
+              end;
+          end;
+      until fin=1;
      end;
 
   begin
@@ -473,12 +681,12 @@ procedure twind.wmpaint(var msg: tmessage);
         setbrush(WindDC,0,color[15],0);
         intro;
         menu;
-        fpool^.insert(new(pstickman,init(WindDC,Hwindow,200,200,1,1)));
+        fpool^.insert(new(pstickman,init(WindDC,Hwindow,300,300,1,0.5)));
         curfighter:=fpool^.at(0);
-        fight; 
+        curfighter^.setkeys1;
         curfighter^.draw;
+        fight;
       end;
-     
   end;
 
 type tapp = object(tapplication)
