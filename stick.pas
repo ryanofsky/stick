808a08e52910375a6af82beaa8d1802a809e9b55
Russell Yanofsky <russ@yanofsky.org>
1999-03-14 05:26:52 +0000

    - work for 1999 science fair

--- stick.pas
+++ stick.pas
@@ -1,13 +1,20 @@
 program stickfighter;
-uses swindow,wobjects;
+uses swindow,wobjects,winprocs;
 
 type tapp = object(tapplication)
        procedure initmainwindow; virtual;
+       procedure initinstance; virtual;
      end;
 
 procedure tapp.initmainwindow;
   begin
     mainwindow:=new(psfwindow,init(nil,'StickFighter'));
+  end;
+
+procedure tapp.initinstance;
+  begin
+    tapplication.initinstance;
+    hacctable := loadaccelerators(hinstance,'STICKMENU');
   end;
 
 var myapp: tapp;
